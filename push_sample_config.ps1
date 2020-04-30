070dea62b8208cd5003bbdd36a1cba5dbe6731b5
nickpupp0 <nppuppo@gmail.com>
2020-04-30 13:02:21 -0400

    Update push_sample_config.ps1

--- push_sample_config.ps1
+++ push_sample_config.ps1
@@ -1,6 +1,8 @@
+# Replace with your own
 $resourceGroup = 'Playground'
 $vmName = 'vm01'
 $storageName = 'playgroundstor01'
 
+# Make sure that your script path is. In my case, I'm running PS in the same directory of the script.
 Publish-AzVMDscConfiguration -ConfigurationPath .\sample_config.ps1 -ResourceGroupName $resourceGroup -StorageAccountName $storageName -force
 Set-AzVMDscExtension -Version '2.80' -ResourceGroupName $resourceGroup -VMName $vmName -ArchiveStorageAccountName $storageName -ArchiveBlobName 'sample_config.ps1.zip' -AutoUpdate -ConfigurationName 'SampleConfig'
